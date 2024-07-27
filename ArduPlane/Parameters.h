8706c121a0a5bb23b343589495b5c37f07aa6664
Michelle Rossouw <michelleros128@gmail.com>
2024-07-27 15:52:11 +1000

    ArduPlane: Added Quicktune
    Thank you Tridge for the help.

--- ArduPlane/Parameters.h
+++ ArduPlane/Parameters.h
@@ -359,6 +359,7 @@ public:
         k_param_autotune_options,
         k_param_takeoff_throttle_min,
         k_param_takeoff_options,
+        k_param_quicktune,
     };
 
     AP_Int16 format_version;
