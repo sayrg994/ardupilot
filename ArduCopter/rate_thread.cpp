535f888e37b106e2020244e403fef59264b7349c
Andy Piper <github@andypiper.com>
2024-08-07 16:56:06 +0100

    Copter: add rate loop config abstraction that allows code to be elided on non-copter builds

--- ArduCopter/rate_thread.cpp
+++ ArduCopter/rate_thread.cpp
@@ -1,7 +1,20 @@
-#include "Copter.h"
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
-#if AP_INERTIALSENSOR_RATE_LOOP_WINDOW_ENABLED && FRAME_CONFIG != HELI_FRAME
-#include <AP_HAL/CondMutex.h>
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "Copter.h"
+#include <AP_InertialSensor/AP_InertialSensor_rate_config.h>
+#if AP_INERTIALSENSOR_RATE_LOOP_WINDOW_ENABLED
 
 #pragma GCC optimize("O2")
 
@@ -27,8 +40,7 @@ void Copter::rate_controller_thread()
     uint32_t rate_loop_count = 0;
     uint32_t prev_loop_count = 0;
 
-    HAL_CondMutex cmutex;
-    ins.set_rate_loop_mutex(&cmutex);
+    ins.enable_fast_rate_buffer();
     hal.rcout->set_dshot_rate(SRV_Channels::get_dshot_rate(), ins.get_raw_gyro_rate_hz() / rate_decimation);
 
     uint32_t last_run_us = AP_HAL::micros();
